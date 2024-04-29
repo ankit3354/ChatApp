8b714cfdf0ddb790f6363584ba7d5cc6a6eca54c
Ankit Vishwakarma <ankitvishwakarma3354@gmail.com>
2024-04-29 19:57:59 +0530

    update Conf

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,12 +1,17 @@
 import { Client, Databases, Account } from "appwrite";
 
-export const PROJECT_ID = "662cd6af001288f95c9a";
-export const DATABASES_ID = "662cd6cc00076498fc87";
-export const COLLECTION_MESSAGE_ID = "662cd6dc0010563f5885";
+export const appwriteProjectURL =
+  import.meta.env.VITE_APPWRITE_URL || "https://cloud.appwrite.io/v1";
+export const PROJECT_ID =
+  import.meta.env.VITE_APPWRITE_PROJECT_ID || "662cd6af001288f95c9a";
+export const DATABASES_ID =
+  import.meta.env.VITE_APPWRITE_DATABASES_ID || "662cd6cc00076498fc87";
+export const COLLECTION_MESSAGE_ID =
+  import.meta.env.VITE_APPWRITE_COLLECTION_MESSAGE_ID || "662cd6dc0010563f5885";
 
 const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("662cd6af001288f95c9a");
+  .setEndpoint(appwriteProjectURL)
+  .setProject(PROJECT_ID);
 
 export const databases = new Databases(client);
 export const account = new Account(client);
