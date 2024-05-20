71bf4e89c994aaf0cd890da7913d74049e0b236d
Ankit Vishwakarma <ankitvishwakarma3354@gmail.com>
2024-05-20 15:45:37 +0530

    new

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -2,10 +2,13 @@ import { Client, Databases, Account } from "appwrite";
 
 export const appwriteProjectURL =
   import.meta.env.VITE_APPWRITE_URL || "https://cloud.appwrite.io/v1";
+
 export const PROJECT_ID =
   import.meta.env.VITE_APPWRITE_PROJECT_ID || "662cd6af001288f95c9a";
+
 export const DATABASES_ID =
   import.meta.env.VITE_APPWRITE_DATABASES_ID || "662cd6cc00076498fc87";
+
 export const COLLECTION_MESSAGE_ID =
   import.meta.env.VITE_APPWRITE_COLLECTION_MESSAGE_ID || "662cd6dc0010563f5885";
 
